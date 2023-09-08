452671ff3ff52f151e49ba9571bc024c9518047d
Daniel Castillo <dlcastillo3015@gmail.com>
2023-09-08 15:59:43 -0400

    ci: config vite library mode for a react hook library with typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/hooks/index.ts"),
+      name: "hook-crafter",
+      formats: ["es", "umd"],
+      fileName: (format) => `hook-crafter.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "styled-components"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "styled-components": "styled",
+        },
+      },
+    },
+  },
 });
