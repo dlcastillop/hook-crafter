36044ae160e9af4e897a90170d3b9bee25f3837b
Daniel Castillo <dlcastillo3015@gmail.com>
2023-12-18 14:39:57 -0500

    build: new setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import { useCountUp } from "./hooks";
 
-function App() {
+const App = () => {
   const { count, increment } = useCountUp(2);
 
-  return (
-    <>
-      <h1>Hook Crafter</h1>
-      <button onClick={increment}>count is {count}</button>
-      <p>
-        Read the{" "}
-        <a
-          href="https://github.com/dlcastillop/hook-crafter/blob/main/README.md"
-          target="_blank"
-        >
-          <code>README.md</code>
-        </a>{" "}
-        file to learn more
-      </p>
-    </>
-  );
-}
+  return <button onClick={increment}>count is {count}</button>;
+};
 
 export default App;
