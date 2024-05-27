553e038a3e35b47161e80386382afa897a08e21e
Daniel Castillo <dlcastillo3015@gmail.com>
2024-05-27 08:31:06 -0400

    feat: clone repo #10

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const readline = require("readline");
+const degit = require("degit");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+const emitter = degit("https://github.com/dlcastillop/hook-crafter");
+
 rl.question("Project name: » ", (name) => {
+  console.log("⬇️  Cloning repository...");
+
+  emitter.clone(name).then(() => {
+    console.log("✅ The repository has been cloned.");
+  });
+
   rl.close();
 });
