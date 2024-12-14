70d7dca61247e7cde3de1d083afc1d86c6ae1c08
Daniel Castillo <dlcastillo3015@gmail.com>
2024-12-14 16:04:33 -0500

    feat: copy template to indicated path #10

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,35 @@ import { Command } from "commander";
 import { copy } from "fs-extra";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
+import prompts from "prompts";
+import { logger } from "./utils/logger";
 
 process.on("SIGINT", () => process.exit(0));
 process.on("SIGTERM", () => process.exit(0));
 
 async function main() {
   const program = new Command()
-    .name("@novajslabs/create-hook-crafter")
+    .name("create-hook-crafter")
     .description("Set up your React hook library in seconds")
     .action(async () => {
       const __dirname = dirname(fileURLToPath(import.meta.url));
       const templatePath = resolve(__dirname, "../template");
-      const destinationPath = resolve(process.cwd(), "./");
+
+      const { path }: { path: string } = await prompts({
+        type: "text",
+        name: "path",
+        message: "Project name:",
+        instructions: false,
+      });
+
+      const destinationPath = resolve(process.cwd(), `${path}`);
+      const pathSplitted = path.split("/");
+      const projectName = pathSplitted[pathSplitted.length - 1];
 
       try {
         await copy(templatePath, destinationPath);
-        console.log("Template copied successfully!");
-      } catch (error) {
-        console.error("Error copying template:", error);
+      } catch (e) {
+        logger.error("Error copying template. Exiting.");
       }
     });
 
