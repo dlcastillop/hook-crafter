00067a04953bd5e15db567da9923095a09c2dea1
Daniel Castillo <dlcastillo3015@gmail.com>
2024-12-15 07:12:14 -0500

    feat: update package.json name on template

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import { copy } from "fs-extra";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 import prompts from "prompts";
-import { logger } from "./utils/logger";
+import { logger } from "@/src/utils/logger";
+import { updatePackageName } from "@/src/utils/package-actions";
 
 process.on("SIGINT", () => process.exit(0));
 process.on("SIGTERM", () => process.exit(0));
@@ -14,9 +15,6 @@ async function main() {
     .name("create-hook-crafter")
     .description("Set up your React hook library in seconds")
     .action(async () => {
-      const __dirname = dirname(fileURLToPath(import.meta.url));
-      const templatePath = resolve(__dirname, "../template");
-
       const { path }: { path: string } = await prompts({
         type: "text",
         name: "path",
@@ -24,7 +22,10 @@ async function main() {
         instructions: false,
       });
 
+      const __dirname = dirname(fileURLToPath(import.meta.url));
+      const templatePath = resolve(__dirname, "../template");
       const destinationPath = resolve(process.cwd(), `${path}`);
+
       const pathSplitted = path.split("/");
       const projectName = pathSplitted[pathSplitted.length - 1];
 
@@ -33,6 +34,8 @@ async function main() {
       } catch (e) {
         logger.error("Error copying template. Exiting.");
       }
+
+      updatePackageName(projectName, destinationPath);
     });
 
   program.parse();
